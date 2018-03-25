2d3a82b9e451367b0332247ca456479384e8174b
mhycy <mhycy@vip.qq.com>
2018-03-25 17:35:09 +0800

    update apt source

--- system-init/ubuntu/switch-apt-jp.sh
+++ system-init/ubuntu/switch-apt-jp.sh
@@ -4,20 +4,21 @@ cat>/etc/apt/sources.list<<EOF
 deb http://jp.archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse
 deb http://jp.archive.ubuntu.com/ubuntu/ xenial-security main restricted universe multiverse
 deb http://jp.archive.ubuntu.com/ubuntu/ xenial-updates main restricted universe multiverse
-deb http://jp.archive.ubuntu.com/ubuntu/ xenial-proposed main restricted universe multiverse
 deb http://jp.archive.ubuntu.com/ubuntu/ xenial-backports main restricted universe multiverse
-deb-src http://jp.archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse
-deb-src http://jp.archive.ubuntu.com/ubuntu/ xenial-security main restricted universe multiverse
-deb-src http://jp.archive.ubuntu.com/ubuntu/ xenial-updates main restricted universe multiverse
-deb-src http://jp.archive.ubuntu.com/ubuntu/ xenial-proposed main restricted universe multiverse
-deb-src http://jp.archive.ubuntu.com/ubuntu/ xenial-backports main restricted universe multiverse
+# deb http://jp.archive.ubuntu.com/ubuntu/ xenial-proposed main restricted universe multiverse
+
+# deb-src http://jp.archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse
+# deb-src http://jp.archive.ubuntu.com/ubuntu/ xenial-security main restricted universe multiverse
+# deb-src http://jp.archive.ubuntu.com/ubuntu/ xenial-updates main restricted universe multiverse
+# deb-src http://jp.archive.ubuntu.com/ubuntu/ xenial-backports main restricted universe multiverse
+# deb-src http://jp.archive.ubuntu.com/ubuntu/ xenial-proposed main restricted universe multiverse
 
 deb http://security.ubuntu.com/ubuntu/ xenial-security main restricted universe multiverse
-deb-src http://security.ubuntu.com/ubuntu/ xenial-security main restricted universe multiverse
+# deb-src http://security.ubuntu.com/ubuntu/ xenial-security main restricted universe multiverse
 
-deb http://archive.canonical.com/ubuntu xenial partner
-deb-src http://archive.canonical.com/ubuntu xenial partner
+# deb http://archive.canonical.com/ubuntu xenial partner
+# deb-src http://archive.canonical.com/ubuntu xenial partner
 
-#deb http://extras.ubuntu.com/ubuntu xenial main
-#deb-src http://extras.ubuntu.com/ubuntu xenial main
+# deb http://extras.ubuntu.com/ubuntu xenial main
+# deb-src http://extras.ubuntu.com/ubuntu xenial main
 EOF
---
+++ system-init/ubuntu/switch-apt-ustcs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+mv /etc/apt/sources.list /etc/apt/sources.list.bak
+cat>/etc/apt/sources.list<<EOF
+deb http://mirrors.ustc.edu.cn/ubuntu/ xenial main restricted universe multiverse
+deb http://mirrors.ustc.edu.cn/ubuntu/ xenial-security main restricted universe multiverse
+deb http://mirrors.ustc.edu.cn/ubuntu/ xenial-updates main restricted universe multiverse
+deb http://mirrors.ustc.edu.cn/ubuntu/ xenial-backports main restricted universe multiverse
+# deb http://mirrors.ustc.edu.cn/ubuntu/ xenial-proposed main restricted universe multiverse
+
+# deb-src http://mirrors.ustc.edu.cn/ubuntu/ xenial main restricted universe multiverse
+# deb-src http://mirrors.ustc.edu.cn/ubuntu/ xenial-security main restricted universe multiverse
+# deb-src http://mirrors.ustc.edu.cn/ubuntu/ xenial-updates main restricted universe multiverse
+# deb-src http://mirrors.ustc.edu.cn/ubuntu/ xenial-backports main restricted universe multiverse
+# deb-src http://mirrors.ustc.edu.cn/ubuntu/ xenial-proposed main restricted universe multiverse
+
+deb http://security.ubuntu.com/ubuntu/ xenial-security main restricted universe multiverse
+# deb-src http://security.ubuntu.com/ubuntu/ xenial-security main restricted universe multiverse
+
+# deb http://archive.canonical.com/ubuntu xenial partner
+# deb-src http://archive.canonical.com/ubuntu xenial partner
+
+# deb http://extras.ubuntu.com/ubuntu xenial main
+# deb-src http://extras.ubuntu.com/ubuntu xenial main
+EOF
